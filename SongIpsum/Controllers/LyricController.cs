f1a14addbe23cde3227d42fa78d6cc89e33a0474
Isabel Ramos <iramos1226@gmail.com>
2017-12-15 22:48:11 -0600

    selectedDecade semi working

--- SongIpsum/Controllers/LyricController.cs
+++ SongIpsum/Controllers/LyricController.cs
@@ -8,10 +8,10 @@ using SongIpsum.Models;
 
 namespace SongIpsum.Controllers
 {
-   /* [RoutePrefix("api/lyric")] */
+    [RoutePrefix("api/lyric")]
     public class LyricController : ApiController
     {
-        [HttpGet, Route("api/lyric")]
+        [HttpGet, Route("")]
         public HttpResponseMessage GetLyrics()
         {
             var db = new ApplicationDbContext();
@@ -19,6 +19,26 @@ namespace SongIpsum.Controllers
             var lyrics = db.Lyric;
 
             return Request.CreateResponse(HttpStatusCode.OK, lyrics);
+        }
+
+        //[HttpGet, Route("api/lyric/decade")]
+        //public HttpResponseMessage GetAllDecades()
+        //{
+        //    var db = new ApplicationDbContext();
+
+        //    var allDecades = db.Lyric;
+
+        //    return Request.CreateResponse(HttpStatusCode.OK, allDecades);
+        //}
+
+        [HttpGet, Route("decade/{Decade}")]
+        public HttpResponseMessage GetDecade(int Decade)
+        {
+            var db = new ApplicationDbContext();
+
+            var decades = db.Lyric.Where(decade => decade.Decade == Decade);
+
+            return Request.CreateResponse(HttpStatusCode.OK, decades);
         }
 
     }
--- SongIpsum/app/controllers/createIpsumCtrl.js
+++ SongIpsum/app/controllers/createIpsumCtrl.js
@@ -1,16 +1,40 @@
 ﻿app.controller("createIpsumCtrl", ["$http", "$scope", function ($http, $scope) {
     $scope.lyricInfo = [];
+    $scope.decadeInfo = [];
+    $scope.selectedDecadeInfo = [];
 
     let getLyrics = () => {
         $http.get("/api/lyric")
             .then((result) => {
                 $scope.lyricInfo = result.data;
             }).catch((error) => {
-                console.log("error", error);
+                console.log("getLyrics error", error);
             });
     };
 
     getLyrics();
+
+    //let getAllDecades = () => {
+    //    $http.get("/api/lyric")
+    //        .then((result) => {
+    //            $scope.decadeInfo = result.data;
+    //        }).catch((error) => {
+    //            console.log("getAllDecades error", error);
+    //        });
+    //};
+
+    //getAllDecades();
+
+    let getSelectedDecade = (selectedDecade) => {
+        $http.get("/api/lyric/decade/{selectedDecade}")
+            .then((result) => {
+                $scope.selectedDecadeInfo = result.data;
+            }).catch((error) => {
+                console.log("getSelectedDecade error", error);
+            });
+    };
+
+
 
 
 }]);
